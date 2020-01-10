d565c55c8ac9c53eeab59e2b87c6c6b22683db7e
kbyyd24 <melo@gaoyuexiang.cn>
2020-01-10 21:36:15 +0800

    implement return fizz buzz whizz when the number is divided by 3*5*7 and doesn't contain 3, 5, 7

--- src/main/java/com/thoughtworks/school/practice/fizzbuzz/FizzBuzzUtil.java
+++ src/main/java/com/thoughtworks/school/practice/fizzbuzz/FizzBuzzUtil.java
@@ -3,6 +3,9 @@ package com.thoughtworks.school.practice.fizzbuzz;
 public class FizzBuzzUtil {
 
   public static String fizzbuzz(int number) {
+    if (number % 3 == 0 && number % 5 == 0 && number % 7 == 0) {
+      return "FizzBuzzWhizz";
+    }
     if (number % 3 == 0 && number % 5 == 0) {
       return "FizzBuzz";
     }
