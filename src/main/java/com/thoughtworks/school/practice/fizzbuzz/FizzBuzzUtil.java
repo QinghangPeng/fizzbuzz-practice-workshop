11d4ab0efedaafe3b4e9df2efcb644798d2f6c89
kbyyd24 <melo@gaoyuexiang.cn>
2020-01-11 00:10:43 +0800

    refactor, chain of responsibility pattern

--- src/main/java/com/thoughtworks/school/practice/fizzbuzz/FizzBuzzUtil.java
+++ src/main/java/com/thoughtworks/school/practice/fizzbuzz/FizzBuzzUtil.java
@@ -1,94 +1,116 @@
 package com.thoughtworks.school.practice.fizzbuzz;
 
-import static java.util.Arrays.asList;
-import static java.util.Collections.singletonList;
-
-import java.util.List;
-import java.util.function.Predicate;
-
 public class FizzBuzzUtil {
 
   private static final String FIZZ = "Fizz";
   private static final String BUZZ = "Buzz";
   private static final String WHIZZ = "Whizz";
 
-  private static final Strategy MAP_3_TO_FIZZ = new Strategy(i -> i % 3 == 0, FIZZ);
-  private static final Strategy MAP_5_TO_BUZZ = new Strategy(i -> i % 5 == 0, BUZZ);
-  private static final Strategy MAP_7_TO_WHIZZ = new Strategy(i -> i % 7 == 0, WHIZZ);
-  private static final Strategy CONTAINS_3_TO_FIZZ = new Strategy(i -> String.valueOf(i).contains("3"), FIZZ);
-
-  private static final Predicate<Integer> CONTAINS_3 = i -> String.valueOf(i).contains("3");
-  private static final Predicate<Integer> CONTAINS_5 = i -> String.valueOf(i).contains("5");
-  private static final Predicate<Integer> CONTAINS_7_AND_3 = i -> {
-    String intStr = String.valueOf(i);
-    return intStr.contains("7") && CONTAINS_3.test(i);
-  };
-  private static final Predicate<Integer> CONTAINS_7_NOT_3 = i -> {
-    String intStr = String.valueOf(i);
-    return intStr.contains("7") && !CONTAINS_3.test(i);
-  };
-
   public static String fizzbuzz(int number) {
-    return buildStrategySelector().stream()
-        .filter(mapper -> mapper.condition.test(number))
-        .findFirst()
-        .map(ConditionStrategyMapper::getStrategies)
-        .orElse(asList(MAP_3_TO_FIZZ, MAP_5_TO_BUZZ, MAP_7_TO_WHIZZ))
-        .stream()
-        .filter(strategy -> strategy.rule.test(number))
-        .map(Strategy::getValue)
-        .reduce((acc, next) -> acc + next)
-        .orElse(String.valueOf(number));
+
+    AbstractFizzBuzzChain fizzbuzzChain = new Contains3(new DividedBy3(new DividedBy5(new DividedBy7(null))));
+
+    String result = fizzbuzzChain.doChain(number);
+
+    return result.length() == 0 ? String.valueOf(number) : result;
+
   }
 
-  private static List<ConditionStrategyMapper> buildStrategySelector() {
-    return asList(
-        new ConditionStrategyMapper(
-            CONTAINS_7_AND_3,
-            singletonList(CONTAINS_3_TO_FIZZ)
-        ),
-        new ConditionStrategyMapper(
-            CONTAINS_7_NOT_3,
-            asList(MAP_3_TO_FIZZ, MAP_7_TO_WHIZZ)
-        ),
-        new ConditionStrategyMapper(
-            CONTAINS_5,
-            asList(MAP_5_TO_BUZZ, MAP_7_TO_WHIZZ)
-        ),
-        new ConditionStrategyMapper(
-            CONTAINS_3,
-            singletonList(CONTAINS_3_TO_FIZZ)
-        ));
-  }
+  private static abstract class AbstractFizzBuzzChain {
 
-  private static class ConditionStrategyMapper {
+    private String value;
+    private AbstractFizzBuzzChain next;
 
-    Predicate<Integer> condition;
-    List<Strategy> strategies;
-
-    public ConditionStrategyMapper(Predicate<Integer> condition,
-        List<Strategy> strategies) {
-      this.condition = condition;
-      this.strategies = strategies;
-    }
-
-    public List<Strategy> getStrategies() {
-      return strategies;
-    }
-  }
-
-  private static class Strategy {
-
-    Predicate<Integer> rule;
-    String value;
-
-    public Strategy(Predicate<Integer> rule, String value) {
-      this.rule = rule;
+    public AbstractFizzBuzzChain(String value, AbstractFizzBuzzChain next) {
       this.value = value;
+      this.next = next;
     }
 
-    public String getValue() {
-      return value;
+    public String doChain(int number) {
+      String currentChainResult = isEnabled(number) && isSatisfyCondition(number) ? value : "";
+      return currentChainResult + this.next(number);
+    }
+
+    abstract protected boolean isEnabled(int number);
+
+    abstract protected boolean isSatisfyCondition(int number);
+
+    private String next(int number) {
+      return this.next == null ? "" : this.next.doChain(number);
     }
   }
+
+  private static class Contains3 extends AbstractFizzBuzzChain {
+
+    public Contains3(AbstractFizzBuzzChain next) {
+      super(FIZZ, next);
+    }
+
+    @Override
+    protected boolean isEnabled(int number) {
+      String numberString = String.valueOf(number);
+      return numberString.contains("7") || !numberString.contains("5");
+    }
+
+    @Override
+    protected boolean isSatisfyCondition(int number) {
+      return String.valueOf(number).contains("3");
+    }
+  }
+
+  private static class DividedBy3 extends AbstractFizzBuzzChain {
+
+    public DividedBy3(AbstractFizzBuzzChain next) {
+      super(FIZZ, next);
+    }
+
+    @Override
+    protected boolean isEnabled(int number) {
+      String stringNumber = String.valueOf(number);
+      return !stringNumber.contains("3") && (stringNumber.contains("7") || !stringNumber.contains("5"));
+    }
+
+    @Override
+    protected boolean isSatisfyCondition(int number) {
+      return number % 3 == 0;
+    }
+  }
+
+  private static class DividedBy5 extends AbstractFizzBuzzChain {
+
+    public DividedBy5(AbstractFizzBuzzChain next) {
+      super(BUZZ, next);
+    }
+
+    @Override
+    protected boolean isEnabled(int number) {
+      String numberString = String.valueOf(number);
+      return !numberString.contains("7") && (numberString.contains("5") || !numberString.contains("3"));
+    }
+
+    @Override
+    protected boolean isSatisfyCondition(int number) {
+      return number % 5 == 0;
+    }
+  }
+
+  private static class DividedBy7 extends AbstractFizzBuzzChain {
+
+    public DividedBy7(AbstractFizzBuzzChain next) {
+      super(WHIZZ, next);
+    }
+
+    @Override
+    protected boolean isEnabled(int number) {
+      String numberString = String.valueOf(number);
+      return !numberString.contains("3") || (numberString.contains("5") && !numberString.contains("7"));
+    }
+
+    @Override
+    protected boolean isSatisfyCondition(int number) {
+      return number % 7 == 0;
+    }
+  }
+
+
 }
