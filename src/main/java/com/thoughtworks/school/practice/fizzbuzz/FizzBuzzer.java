1de317b4719c4088777c738ce00c8cc2317dfdec
kbyyd24 <melo@gaoyuexiang.cn>
2020-01-12 00:27:27 +0800

    refactor

--- src/main/java/com/thoughtworks/school/practice/fizzbuzz/FizzBuzzer.java
+++ src/main/java/com/thoughtworks/school/practice/fizzbuzz/FizzBuzzer.java
@@ -1,7 +1,9 @@
 package com.thoughtworks.school.practice.fizzbuzz;
 
-import java.util.ArrayList;
+import static java.util.Arrays.asList;
+
 import java.util.List;
+import java.util.function.Predicate;
 
 public class FizzBuzzer {
 
@@ -9,32 +11,64 @@ public class FizzBuzzer {
   private static final String BUZZ = "Buzz";
   private static final String WHIZZ = "Whizz";
 
+  private List<FizzBuzzRuler> rulers;
+
+  public FizzBuzzer() {
+    rulers = asList(
+        new FizzBuzzRuler(
+            digit -> isContains(digit, 7) || !isContains(digit, 5),
+            digit -> isContains(digit, 3),
+            FIZZ
+        ),
+        new FizzBuzzRuler(
+            digit -> (isContains(digit, 7) || !isContains(digit, 5)) && !isContains(digit, 3),
+            digit -> isMultipleOf(digit, 3),
+            FIZZ
+        ),
+        new FizzBuzzRuler(
+            digit -> !isContains(digit, 7) && (isContains(digit, 5) || !isContains(digit, 3)),
+            digit -> isMultipleOf(digit, 5),
+            BUZZ
+        ),
+        new FizzBuzzRuler(
+            digit -> isContains(digit, 5) || !isContains(digit, 3),
+            digit -> isMultipleOf(digit, 7),
+            WHIZZ
+        )
+    );
+  }
+
+  private boolean isContains(Integer digit, Integer contained) {
+    return digit.toString().contains(contained.toString());
+  }
+
   public String fizzBuzz(int digit) {
-    List<String> result = new ArrayList<>();
-    String digitString = String.valueOf(digit);
-    boolean contains3 = digitString.contains("3");
-    boolean contains5 = digitString.contains("5");
-    boolean contains7 = digitString.contains("7");
-    boolean enableRulesOf3 = contains7 || !contains5;
-    boolean enableDividedBy3 = enableRulesOf3 && !contains3;
-    boolean enableDividedBy7 = contains5 || !contains3;
-    boolean enableDividedBy5 = !contains7 && enableDividedBy7;
-    if (enableRulesOf3 && contains3) {
-      result.add(FIZZ);
-    }
-    if (enableDividedBy3 && isMultipleOf(digit, 3)) {
-      result.add(FIZZ);
-    }
-    if (enableDividedBy5 && isMultipleOf(digit, 5)) {
-      result.add(BUZZ);
-    }
-    if (enableDividedBy7 && isMultipleOf(digit, 7)) {
-      result.add(WHIZZ);
-    }
-    return result.isEmpty() ? digitString : String.join("", result);
+    return rulers.stream()
+        .filter(rule -> rule.enableCondition.test(digit))
+        .filter(rule -> rule.ruleChecker.test(digit))
+        .map(FizzBuzzRuler::getValue)
+        .reduce((acc, next) -> acc + next)
+        .orElseGet(() -> String.valueOf(digit));
   }
 
   private boolean isMultipleOf(int digit, int dividedBy) {
     return digit % dividedBy == 0;
+  }
+
+  private static class FizzBuzzRuler {
+
+    Predicate<Integer> enableCondition;
+    Predicate<Integer> ruleChecker;
+    String value;
+
+    public FizzBuzzRuler(Predicate<Integer> enableCondition, Predicate<Integer> ruleChecker, String value) {
+      this.enableCondition = enableCondition;
+      this.ruleChecker = ruleChecker;
+      this.value = value;
+    }
+
+    public String getValue() {
+      return value;
+    }
   }
 }
