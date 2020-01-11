579993c955e09a8bb09db7e7d422f99b6b31ea5a
kbyyd24 <melo@gaoyuexiang.cn>
2020-01-11 23:51:13 +0800

    fixed input not matched tasking

--- src/test/java/com/thoughtworks/school/practice/fizzbuzz/FizzBuzzerTest.java
+++ src/test/java/com/thoughtworks/school/practice/fizzbuzz/FizzBuzzerTest.java
@@ -20,12 +20,12 @@ class FizzBuzzerTest {
 
   @Test
   void should_return_buzz_when_it_is_multiple_of_5() {
-    assertEquals("Buzz", fizzBuzzer.fizzBuzz(5));
+    assertEquals("Buzz", fizzBuzzer.fizzBuzz(10));
   }
 
   @Test
   void should_return_whizz_when_it_is_multiple_of_7() {
-    assertEquals("Whizz", fizzBuzzer.fizzBuzz(7));
+    assertEquals("Whizz", fizzBuzzer.fizzBuzz(14));
   }
 
   @Test
