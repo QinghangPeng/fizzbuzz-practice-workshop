783a5572c89b789fd0dfeea80cbf5e4b10703163
kbyyd24 <melo@gaoyuexiang.cn>
2020-01-11 23:40:55 +0800

    implement return fizz when it is multiple of 3

--- src/main/java/com/thoughtworks/school/practice/fizzbuzz/FizzBuzzer.java
+++ src/main/java/com/thoughtworks/school/practice/fizzbuzz/FizzBuzzer.java
@@ -2,7 +2,12 @@ package com.thoughtworks.school.practice.fizzbuzz;
 
 public class FizzBuzzer {
 
+  private static final String FIZZ = "Fizz";
+
   public String fizzBuzz(int digit) {
+    if (digit % 3 == 0) {
+      return FIZZ;
+    }
     return String.valueOf(digit);
   }
 }
--- src/test/java/com/thoughtworks/school/practice/fizzbuzz/FizzBuzzerTest.java
+++ src/test/java/com/thoughtworks/school/practice/fizzbuzz/FizzBuzzerTest.java
@@ -12,4 +12,9 @@ class FizzBuzzerTest {
   void should_return_the_number_when_it_does_not_match_any_rule() {
     assertEquals("2", fizzBuzzer.fizzBuzz(2));
   }
+
+  @Test
+  void should_return_fizz_when_it_is_multiple_of_3() {
+    assertEquals("Fizz", fizzBuzzer.fizzBuzz(3));
+  }
 }
